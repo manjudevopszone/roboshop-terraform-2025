d709331b85aea8bdbe6df1585d22900bdd5aa0b4
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2026-03-07 20:24:17 +0530

    helm deployment

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 provider "vault" {
-  address = "http://vault.devopsbymanju.shop:8200"
+  address = "http://vault.devopsmanju.shop:8200"
   token = var.vault_token
 }
