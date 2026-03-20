64ae1dd8261c75930e25a7a044806714339d6688
Manju9876 <manjunathb960650@gmail.com>
2026-03-20 12:59:40 +0530

    terraform practice

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -48,7 +48,7 @@ resource "aws_eks_addon" "addons" {
 }
 
 resource "null_resource" "kubeconfig" {
-  depends_on = [aws_eks_cluster.main]
+  depends_on = [aws_eks_node_group.main]
 
   provisioner "local-exec" {
     command = "aws eks update-kubeconfig --name ${aws_eks_cluster.main.name}"
@@ -56,12 +56,10 @@ resource "null_resource" "kubeconfig" {
 }
 
 
-resource "null_resource" "metricserver" {
-  depends_on = [aws_eks_cluster.main]
+resource "null_resource" "metrics-server" {
+  depends_on = [null_resource.kubeconfig]
 
   provisioner "local-exec" {
-    command = "kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.8.1/components.yaml"
+    command = "kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml"
   }
 }
-
-
